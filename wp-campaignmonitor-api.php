f74437ffc55c05c96d364dc999201600403c8bd6
Ari Berkowicz <applemaster.ari@gmail.com>
2017-07-12 11:04:56 -0700

    Added all the functions that are for CampaignMonitor's API into its own PHP file

--- wp-campaignmonitor-api.php
+++ wp-campaignmonitor-api.php
@@ -118,7 +118,7 @@ if ( ! class_exists( 'BasecampAPI' ) ) {
 
 		}
 
-		public function update_administrator() {
+		public function set_administrator($email) {
 
 		}
 
@@ -126,19 +126,19 @@ if ( ! class_exists( 'BasecampAPI' ) ) {
 
 		}
 
-		public function get_admin_details() {
+		public function get_admin_details($email) {
 
 		}
 
-		public function delete_admin() {
+		public function delete_admin($email) {
 
 		}
 
-		public function set_primary_contact() {
+		public function set_primary_account($email) {
 
 		}
 
-		public function get_primary_contact() {
+		public function get_primary_account() {
 
 		}
 
@@ -161,17 +161,349 @@ if ( ! class_exists( 'BasecampAPI' ) ) {
 
 		}
 
+		public function send_campaign_draft($campaign_id){
+
+		}
+
+		public function send_campaign_preview($campaign_id){
+
+		}
+
+		public function get_campaign_summary($campaign_id){
+
+		}
+
+		public function get_list_campaign_client_emails($campaign_id){
+
+		}
+
+		public function get_lists_and_segments_campaign($campaign_id){
+
+		}
+		public function get_campaign_recipients($campaign_id, $page_number = 1, $page_size = 1000, $order_field = 'email', $order_direction = 'asc'){
+
+		}
+
+		public function get_campaign_bounces( $campaign_id, $date = '', $page = 1, $page_size = 1000, $order_field = 'email', $order_direction = 'asc' ) {
+
+		}
+
+		public function get_campaign_opens($campaign_id, $date = '', $page_number = 1, $page_size = 1000, $order_field = 'email', $order_direction = 'asc'){
+
+		}
+
+		public function get_campaign_clicks($campaign_id, $date = '', $page_number = 1, $page_size = 1000, $order_field = 'email', $order_direction = 'asc'){
+
+		}
+
+		public function get_campaign_unsubscribes($campaign_id, $date = '', $page_number = 1, $page_size = 1000, $order_field = 'email', $order_direction = 'asc'){
+
+		}
+
+		public function get_campaign_spam_complaints($campaign_id, $date = '', $page_number = 1, $page_size = 1000, $order_field = 'email', $order_direction = 'asc'){
+
+		}
+
+		public function delete_campaign($campaign_id){
+
+		}
+
+		public function unschedule_campaign($campaign_id){
+
+		}
 		/* CLIENTS. */
+		public function add_client(){
+
+		}
+		public function get_client_details($client_id){
+
+		}
+		public function get_sent_campaign($client_id){
+
+		}
+
+		public function get_sschedule_campaign($client_id){
+
+		}
+
+		public function get_draft_campaign($client_id){
+
+		}
+
+		public function get_subscriber_lists($client_id){
+
+		}
+
+		public function get_lists_email_address($client_id, $email){
+
+		}
+
+		public function get_segments($client_id){
+
+		}
+
+		public function get_suppression_list($client_id, $page = 1, $page_size = 1000, $order_field = 'email', $order_direction = 'asc'){
+
+		}
+
+		public function add_suppress_email_address($client_id){
+
+		}
+
+		public function delete_supress_email_address($client_id, $email){
+
+		}
+
+		public function get_templates($client_id){
+
+		}
+
+		public function set_setting_basic_details($client_id){
+
+		}
+
+		public function set_PAYG_billing($client_id){
+
+		}
+
+		public function set_monthly_billing($client_id){
+
+		}
+
+		public function transfer_credits($client_id){
+
+		}
+
+		public function delete_client($client_id){
+
+		}
+
+		public function add_person($client_id){
+
+		}
+
+		public function set_person($client_id, $email){
+
+		}
+
+		public function get_people($client_id){
+
+		}
+
+		public function get_person_details($client_id, $email){
+
+		}
+
+		public function delete_person($client_id, $email){
+
+		}
+
+		public function set_primary_contact($client_id, $email){
+
+		}
+
+		public function get_primary_contact($client_id){
+
+		}
 
 		/* LISTS. */
 
+		public function add_list($client_id){
+
+		}
+
+		public function get_list_details($list_id){
+
+		}
+
+		public function get_list_stats($list_id){
+
+		}
+
+		public function get_list_custom_fields($list_id){
+
+		}
+
+		public function get_list_segments($list_id){
+
+		}
+
+		public function get_active_list_subscribers($list_id, $date = '', $page = 1, $page_size = 1000, $order_field = 'date', $order_direction = 'asc'){
+
+		}
+
+		public function get_unconfirmed_subscribers($list_id, $date = '', $page = 1, $page_size = 1000, $order_field = 'date', $order_direction = 'asc'){
+
+		}
+
+		public function get_bounced_subscriber($list_id, $date = '', $page = 1, $page_size = 1000, $order_field = 'email', $order_direction = 'asc'){
+
+		}
+
+		public function get_deleted_subscribers($list_id, $date = '', $page = 1, $page_size = 1000, $order_field = 'email', $order_direction = 'asc'){
+
+		}
+
+		public function set_list($list_id){
+
+		}
+
+		public function add_custom_field($list_id){
+
+		}
+
+		public function set_custom_field($list_id, $custom_field_key){
+
+		}
+
+		public function set_custom_field_options($list_id, $custom_field_key){
+
+		}
+
+		public function delete_custom_field($list_id, $custom_field_key){
+
+		}
+
+		public function delete_list($list_id){
+
+		}
+
+		public function get_list_webhooks($list_id){
+
+		}
+
+		public function add_webhook($list_id){
+
+		}
+
+		public function get_test_webhook($list_id, $webhook_id){
+
+		}
+
+		public function delete_webhook($list_id, $webhook_id){
+
+		}
+
+		public function add_active_webhook($list_id, $webhook_id){
+
+		}
+
+		public function delete_active_webhook($list_id, $webhook_id){
+
+		}
+
 		/* SEGMENTS. */
+		public function add_segment($list_id){
+
+		}
+
+		public function set_segment($segment_id){
+
+		}
+
+		public function add_segment_rulegroup($segment_id){
+
+		}
+
+		public function get_segment_details($segment_id){
+
+		}
+
+		public function get_active_segment_subscribers($segment_id, $date = '', $page = 1, $page_size = 1000, $order_field = 'date', $order_direction = 'asc'){
+
+		}
+
+		public function delete_segment($segment_id){
+
+		}
+
+		public function delete_segment_rule($segment_id){
+
+		}
 
 		/* SUBSCRIBERS. */
 
+		public function add_subscriber($list_id){
+
+		}
+
+		public function set_subscriber($list_id, $email){
+
+		}
+
+		public function add_subscribers($list_id){
+
+		}
+
+		public function get_subscriber_details($list_id, $email){
+
+		}
+
+		public function get_subscriber_history($list_id, $email){
+
+		}
+
+		public function delete_active_subscriber($list_id){
+
+		}
+
+		public function delete_subscriber($list_id, $email){
+
+		}
+
 		/* TEMPLATES. */
+		public function get_template($template_id){
+
+		}
+
+		public function add_template($template_id){
+
+		}
+
+		public function set_template($template_id){
+
+		}
+
+		public function delete_template($template_id){
+
+		}
 
 		/* TRANSACTIONAL. */
+		public function get_smart_transactional_email_list($status = 'all', $client_id){
+
+		}
+
+		public function get_smart_transactional_email_details($smart_email_id){
+
+		}
+
+		public function send_smart_email($smart_email_id){
+
+		}
+
+		public function send_classic_email($client_id){
+
+		}
+
+		public function get_classic_email_list($client_id){
+
+		}
+
+		public function get_email_statistics($group = '', $from = '2017-06-13', $to = '2017-07-12', $time_zone = 'PCT', $client_id){
+
+		}
+
+		public function get_list_message_timeline($group = '', $sent_before_id = null, $sent_after_id = null, $count = 50, $status = 'all', $client_id){
+
+		}
+
+		public function get_message_details($message_id, $statistics = false){
+
+		}
+
+		public function resend_message($message_id){
+
+		}
 
 		/* WEBHOOKS. */
 
