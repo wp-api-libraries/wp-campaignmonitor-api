5975ffc6329da90731ec2486843ce26c6917073d
Brandon Hubbard <bhubbard@users.noreply.github.com>
2017-07-24 21:37:31 -0700

    Update wp-campaignmonitor-api.php

--- wp-campaignmonitor-api.php
+++ wp-campaignmonitor-api.php
@@ -85,8 +85,6 @@ if ( ! class_exists( 'CampaignMonitorAPI' ) ) {
 
 			$response = wp_remote_request( $request, $this->args );
 
-			var_dump( $response );
-
 			$code = wp_remote_retrieve_response_code( $response );
 			if ( 200 !== $code ) {
 				return new WP_Error( 'response-error', sprintf( __( 'Server response code: %d', 'wp-campaignmonitor-api' ), $code ) );
